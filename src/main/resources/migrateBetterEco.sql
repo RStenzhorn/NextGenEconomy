5dc6ab037272a2ea2c88b8e10f1d2fbcd204bddd
RStenzhorn <robert.stenzhorn@outlook.de>
2023-11-30 23:10:41 +0100

    fixing

--- src/main/resources/migrateBetterEco.sql
+++ src/main/resources/migrateBetterEco.sql
@@ -4,21 +4,6 @@ ALTER TABLE economy
 UPDATE economy
 SET new_uuid_binary = UNHEX(REPLACE(uuid, '-', ''));
 
-ALTER TABLE economyALTER TABLE economy
-    ADD COLUMN new_uuid_binary BINARY (16);
-
-UPDATE economy
-SET new_uuid_binary = UNHEX(REPLACE(uuid, '-', ''));
-
-ALTER TABLE economy
-    DROP COLUMN uuid,
-    CHANGE COLUMN new_uuid_binary uuid BINARY (16);
-
-ALTER TABLE economy
-    MODIFY balance DECIMAL (38, 2) DEFAULT 0 NOT NULL;
-
-ALTER TABLE economy
-    ADD PRIMARY KEY (uuid);
 
 alter table economy
     drop column uuid;
@@ -27,9 +12,9 @@ alter table economy
     change new_uuid_binary uuid binary(16) null;
 
 ALTER TABLE economy
-    MODIFY balance DECIMAL (38, 2) DEFAULT 0 NOT NULL;
+    ADD PRIMARY KEY (uuid);
 
 ALTER TABLE economy
-    ADD PRIMARY KEY (uuid);
+    MODIFY balance DECIMAL (38, 2) DEFAULT 0 NOT NULL;
 
 rename table economy to economy_player;
