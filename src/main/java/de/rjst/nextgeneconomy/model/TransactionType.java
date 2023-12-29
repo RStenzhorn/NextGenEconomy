65d34c349ef0a2e60292090dbdc3ec6d7fab8005
RStenzhorn <robert.stenzhorn@outlook.de>
2023-12-29 22:19:35 +0100

    Refactoring

--- src/main/java/de/rjst/nextgeneconomy/model/TransactionType.java
+++ src/main/java/de/rjst/nextgeneconomy/model/TransactionType.java
@@ -1,19 +1,25 @@
 package de.rjst.nextgeneconomy.model;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.*;
 
+@RequiredArgsConstructor
+@Getter
 public enum TransactionType {
 
-    ADD,
-    SET,
-    REMOVE;
+    ADD("give"),
+    SET("set"),
+    REMOVE("take");
+
+    private final String completions;
 
     public static @NotNull List<String> getCompletions () {
         final List<String> result = new ArrayList<>();
         for (final TransactionType value : values()) {
-            result.add(value.name().toLowerCase(Locale.ROOT));
+            result.add(value.completions);
         }
         return result;
     }
